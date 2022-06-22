0ae0fe51b35187295810a0bccc2e04922402aa37
SolomonOjigbo <solomonojigbo@gmail.com>
2022-06-22 03:00:59 +0100

    Created the Dishdetail component displayed below the menu when a menu dish is clicked.

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
     id: string;
     name: string;
@@ -7,4 +9,17 @@ export class Dish {
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 }
+
+// export class SelectedDish {
+//     id: string;
+//     name: string;
+//     image: string;
+//     category: string;
+//     featured: boolean;
+//     label: string;
+//     price: string;
+//     description: string;
+//     comments: {rating: number, comment: string, author: string, date: string}[]
+// }
