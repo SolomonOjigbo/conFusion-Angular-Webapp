9eb32bccf885ec32c9db599da4ab05ba5d38c8cb
SolomonOjigbo <solomonojigbo@gmail.com>
2022-06-22 19:00:28 +0100

    Designed the about page and added to the header and footer router links to the about component. Also added a featured leader to the Home component

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -11,15 +11,3 @@ export class Dish {
     description: string;
     comments: Comment[];
 }
-
-// export class SelectedDish {
-//     id: string;
-//     name: string;
-//     image: string;
-//     category: string;
-//     featured: boolean;
-//     label: string;
-//     price: string;
-//     description: string;
-//     comments: {rating: number, comment: string, author: string, date: string}[]
-// }
